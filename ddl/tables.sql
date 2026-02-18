d1310eb2f4db20edadcb6e5dafceebeddb212d5b
Mariya Shkurat <57259035+timship@users.noreply.github.com>
2026-02-18 13:04:18 +0300

    Add tables.sql

---
+++ ddl/tables.sql
@@ -0,0 +1,17 @@
+create table routes (
+	flight_no 			char(6) primary key, 		-- номер рейса
+	departure_airport 	char(3),		    	  -- код аэропорта вылета
+	arrival_airport 	char(3),				  -- код аэропорта прилета
+	aircraft_code 		char(3), 			   		--код самолета
+	duration 			interval 					        /*продолжительность полета, 
+													                которая будет рассчитана как разница между запланированными временами вылета и прилета*/
+);
+
+insert into routes (flight_no,departure_airport,arrival_airport,aircraft_code,duration)
+select 
+		distinct flight_no	::char(6),
+		departure_airport 	::char(3),
+		arrival_airport		  ::char(3),
+		aircraft_code 	  	::char(3),
+		(scheduled_arrival - scheduled_departure) ::interval as duration
+from 	bookings.flights	
